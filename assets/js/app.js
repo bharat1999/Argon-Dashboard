711340abbc07bd381f77c4e333c9d80704ae7393
Ankit1028 <photonicvibes10@gmail.com>
2021-07-02 23:00:00 +0530

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,51 @@
+
+
+
+function getData() {
+    axios.get("https://run.mocky.io/v3/d53400a3-6126-495e-9d16-0b4414b537b3")
+      .then(function(r){
+        var str = "}"
+        var res = JSON.parse(r.data.concat(str))
+        // res.json(r)
+        console.log(res)
+        show(res)
+      })
+      .catch(function(e){
+        console.log(e)
+      })
+}
+
+getData()
+
+
+function show(data) {
+    let tab 
+    
+    // Loop to access all rows 
+    for (let r of data.clients) {
+        tab += `<tr>
+        <td>
+          ${r.id}
+        </td>
+        <td>
+        ${r.name}
+        </td>
+        <td>
+            ${r.company}
+        </td>
+        <td>
+            ${r.orderID}
+        </td>
+        <td>
+            ${r.invoicepaid}
+        </td>
+        <td>
+            ${r.invoicePending}
+        </td>
+      </tr>`;
+    }
+    // Setting innerHTML as tab variable
+    console.log("done")
+    document.getElementById("tableBody").innerHTML = tab;
+    document.getElementById("tableBody").classList.remove("d-none")
+}
